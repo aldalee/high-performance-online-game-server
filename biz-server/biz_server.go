4d66f60cf42bcab812856c2baa3cb9ce51250244
aldalee <hadoop_2020@163.com>
2024-05-14 22:29:20 +0800

    implement a simple HTTP server

--- biz-server/biz_server.go
+++ biz-server/biz_server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aldalee/high-performance-online-game-server/comm/log"
+	"net/http"
 	"os"
 	"path"
 )
@@ -13,4 +14,9 @@ func main() {
 	}
 	log.Config(path.Dir(exe) + "/logs/biz-server")
 	log.Info("start business server")
+
+	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello World!"))
+	})
+	_ = http.ListenAndServe("127.0.0.1:80", nil)
 }
